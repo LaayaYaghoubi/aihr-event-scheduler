396116e8dc64af4c1e5275a163b6f418359df717
Laaya Yaghoubi <layayaghoubi@gmail.com>
2025-01-25 02:56:01 +0330

    feat: added welcome component

---
+++ AIHR.Client/src/presentation/components/welcome/welcome.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+@Component({
+  selector: 'app-welcome',
+  imports: [MatToolbar],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.scss'
+})
+export class WelcomeComponent {
+
+}
